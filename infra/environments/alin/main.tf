9511ab216a57c5e461e75bde64d3ea5025c1748a
Alin Plamadeala <alin.plamadeala@outlook.com>
2022-11-22 00:43:51 +0100

    solution 1

--- infra/environments/alin/main.tf
+++ infra/environments/alin/main.tf
@@ -3,3 +3,13 @@ provider "google" {
   region  = var.region
 }
 
+module "cloud_run" {
+  source  = "GoogleCloudPlatform/cloud-run/google"
+  version = "~> 0.2.0"
+
+  service_name           = "my-api-service-2"
+  project_id             = var.project
+  location               = var.region
+  image                  = "gcr.io/cloudrun/hello"
+  members = [ "allUsers" ]
+}
